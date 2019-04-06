373a67020c4f230590da2376151a442bc020dd15
Christopher David <chris@arcade.city>
2019-04-06 15:09:32 -0500

    Save gzipped example payload to file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const ProtoDef = require('protodef').ProtoDef
 const Serializer = require('protodef').Serializer
 const Parser = require('protodef').Parser
+const fs = require('fs')
+const zlib = require('zlib')
 
 const exampleProtocol = require('./arcm.json')
 
@@ -19,14 +21,26 @@ let payload = {
   }
 }
 
-let buff = proto.createPacketBuffer('entity_look', payload.params)
+let testum = proto.createPacketBuffer('entity_look', payload.params)
 
-console.log('buff:', buff)
-//
-// serializer.write(payload)
-//
-// serializer.pipe(parser)
-//
-// parser.on('data', function (chunk) {
-//   console.log(JSON.stringify(chunk, null, 2))
-// })
+console.log('buffer:', testum)
+console.log('-------')
+
+const gzip = zlib.createGzip()
+
+zlib.deflate(testum, (err, buffer) => {
+    if (!err) {
+        console.log(buffer.toString('base64'))
+
+        fs.writeFile('arcd-car8.schematic', buffer, function(error, data) {
+            if (error) {
+                console.log(error)
+            } else {
+                console.log('Success')
+            }
+        })
+
+    } else {
+        console.log('error:', err)
+    }
+})
