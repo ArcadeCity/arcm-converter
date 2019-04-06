a844091917e1f4e5be6c965c38edc62f69230d1f
Christopher David <chris@arcade.city>
2019-04-06 11:47:38 -0500

    Parse car schematic, show js obj

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 var nbt = require('prismarine-nbt')
+var fs = require('fs')
 
-console.log('Test')
-console.log(nbt)
+fs.readFile('arcd-car6.schematic', function(error, data) {
+    if (error) throw error;
+
+    nbt.parse(data, function(error, data) {
+
+        console.log(data)
+
+        // console.log(data.value.stringTest.value);
+        // console.log(data.value['nested compound test'].value);
+    });
+});
