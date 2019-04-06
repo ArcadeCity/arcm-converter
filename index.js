5b1826e759f4eb879f13a2519f7dd11bf673334f
Christopher David <chris@arcade.city>
2019-04-06 14:53:03 -0500

    Lets use .arcm instead of .arca

--- index.js
+++ index.js
@@ -5,34 +5,94 @@ const ProtoDef = require('protodef').ProtoDef
 const Serializer = require('protodef').Serializer
 const Parser = require('protodef').Parser
 
-var model = {
-    type: 'nbt',
-    name: 'Arca Schematic',
-    data: {
-        Width: { // x
-            type: 'short',
-            value: 2
-        },
-        Height: { // y
-            type: 'short',
-            value: 2
-        },
-        Length: { // z
-            type: 'short',
-            value: 2
-        },
-        Blocks: {
-            type: 'byteArray',
-            value: [1,1,1,0,1,1,1,1]
-        }
+const protocol = {
+    'container': 'native',
+'varint': 'native',
+'byte': 'native',
+'bool': 'native',
+'switch': 'native',
+'entity_look': [
+  'container',
+  [
+    {
+      'name': 'entityId',
+      'type': 'varint'
+    },
+    {
+      'name': 'yaw',
+      'type': 'i8'
+    },
+    {
+      'name': 'pitch',
+      'type': 'i8'
+    },
+    {
+      'name': 'onGround',
+      'type': 'bool'
     }
+  ]
+],
+'packet': [
+  'container',
+  [
+    {
+      'name': 'name',
+      'type': [
+        'mapper',
+        {
+          'type': 'varint',
+          'mappings': {
+            '22': 'entity_look'
+          }
+        }
+      ]
+    },
+    {
+      'name': 'params',
+      'type': [
+        'switch',
+        {
+          'compareTo': 'name',
+          'fields': {
+            'entity_look': 'entity_look'
+          }
+        }
+      ]
+    }
+  ]
+]
 }
 
-var testData = {
-    type: 'byteArray',
-    description: '8-bit byte arrays',
-    data: [1,1,1,1]
-}
+// var model = {
+//     type: 'compound',
+//     name: 'Arca Schematic',
+//     value: {
+//         Width: { // x
+//             type: 'short',
+//             value: 2
+//         },
+//         Height: { // y
+//             type: 'short',
+//             value: 2
+//         },
+//         Length: { // z
+//             type: 'short',
+//             value: 2
+//         },
+//         Blocks: {
+//             type: 'byteArray',
+//             value: [1,1,1,0,1,1,1,1]
+//         }
+//     }
+// }
+
+var model = require('./test.js')
+
+// var testData = {
+//     type: 'byteArray',
+//     description: '8-bit byte arrays',
+//     data: [1,1,1,1]
+// }
 
 // const protocol = {
 //
@@ -47,13 +107,15 @@ var testData = {
 
 // console.log(model)
 
-const protocol = JSON.stringify(require('./nbt.json'))
+// const protocol = JSON.stringify(require('./nbt.json'))
 
 const proto = new ProtoDef()
 proto.addTypes(JSON.parse(protocol))
 
 
-let test = proto.createPacketBuffer('byteArray', [1,1,1,1])
+// let test = proto.createPacketBuffer('packet', model)
+
+
 
 console.log(test)
 
