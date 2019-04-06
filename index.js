74603bd4363e11420ad9e32dd484a65fb556a561
Christopher David <chris@arcade.city>
2019-04-06 11:54:22 -0500

    Write uncompressed buffer to file

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ fs.readFile('arcd-car6.schematic', function(error, data) {
 
         console.log(data)
 
+        let testum = nbt.writeUncompressed(data)
+
+        console.log(testum)
+
+
+        fs.writeFile('arcd-car7.schematic', testum, function(error, data) {
+            if (error) {
+                console.log(error)
+            } else {
+                console.log('Success')
+            }
+        })
+
         // console.log(data.value.stringTest.value);
         // console.log(data.value['nested compound test'].value);
     });
