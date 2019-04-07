94b6fcf9057bcd34170f169a99126b4de2a12fd0
Christopher David <chris@arcade.city>
2019-04-06 22:14:52 -0500

    Flip the .vox y and z axes to align w babylon

--- index-vox.js
+++ index-vox.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const VoxReader = require('@sh-dave/format-vox').VoxReader;
 
-fs.readFile("./car.vox", function (err, buffer) {
+const file = 'car.vox' // says x 50, y 100, z 23 ... but should be  x 50, y 23, z 100
+
+fs.readFile("./" + file, function (err, buffer) {
   if (err) throw err;
 
   VoxReader.read(buffer, (vox, err) => {
@@ -9,7 +11,44 @@ fs.readFile("./car.vox", function (err, buffer) {
           console.error(err)
           return
       } else {
-          console.log(vox)
+          // console.log(vox)
+          // console.log('sizes:', vox.sizes)
+          // console.log('palette:', vox.palette)
+          // console.log('a model:', vox.models[0][0])
+
+          var models = vox.models[0]
+
+          // First flip the y and z axes
+          models.forEach(model => {
+              let nowY = model.y
+              let nowZ = model.z
+
+              model.z = nowY
+              model.y = nowZ
+          })
+
+          // Then sort by y, z, x
+
+          // models.sort(
+          //     function(a, b) {
+          //         if (a.y === b.y) {
+          //             return a.z - b.z
+          //         }
+          //         console.log('hm')
+          //         return a.y > b.y ? 1 : -1
+          //     }
+          // )
+
+          console.log(models)
+
+          console.log(file + ' has ' + models.length + ' voxels.')
+          console.log(vox.sizes[0])
+          console.log('actual: z is ' + vox.sizes[0].y + ' and y is ' + vox.sizes[0].z)
+
+
+          // Sort this array to be in ordered by
+
+
       }
   })
 
